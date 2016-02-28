ffc4d99f766dd14d40b86d47e3d01758fcff7a20
dev017 <dev017@192.168.1.3>
2016-02-28 21:49:44 +0000

    Exemple d'utilisation de Spring AOP

---
+++ src/com/formation/spring/aop/LoggerAdherent.java
@@ -0,0 +1,28 @@
+package com.formation.spring.aop;
+
+import org.aspectj.lang.JoinPoint.StaticPart;
+
+
+
+public class LoggerAdherent {
+
+	public void logDebutMethode(org.aspectj.lang.JoinPoint joinPoint) {
+		final Object[] args = joinPoint.getArgs();
+		final StringBuffer sb = new StringBuffer();
+		sb.append(joinPoint.getSignature().toString());
+		sb.append("avec les parametres : (");
+		for (int i = 0; i < args.length; i++) {
+			sb.append(args[i]);
+			if (i < args.length - 1) {
+				sb.append(", ");
+			}
+		}
+		sb.append(")");
+		System.out.println(sb);
+	}
+
+	public void logFinMethode(StaticPart staticPart, Object result) {
+		String name = staticPart.getSignature().toLongString();
+		System.out.println(name + " returning :[" + result + "]");
+	}
+}
---
+++ src/com/formation/spring/service/ServiceMetier.java
@@ -0,0 +1,10 @@
+package com.formation.spring.service;
+
+public class ServiceMetier {
+
+	public int ajouterAdherent() {
+		System.out.println("Opération d'ajout d'un adherent");
+		int nbre = 100;
+		return nbre;
+	}
+}
